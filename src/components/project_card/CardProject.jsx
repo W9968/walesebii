5aaedfc9d3536bf57879f308b401387eebcf4c80
Walee Sebiiy <44040669+jagarkin@users.noreply.github.com>
2021-07-03 20:14:54 +0100

    card

--- src/components/project_card/CardProject.jsx
+++ src/components/project_card/CardProject.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import styled from 'styled-components'
+import { motion } from 'framer-motion'
 
 const CardProject = () => {
   return (
     <>
       <Container>
-        <Title>hello</Title>
-        <Parag>test parag</Parag>
+        <Image src='./asset/colorpicker.png' />
+        <Overly initial={{ opacity: 0 }} whileHover={{ opacity: 1 }}>
+          <motion.div>
+            <Title>hello</Title>
+            <Parag>hello Hello</Parag>
+          </motion.div>
+        </Overly>
       </Container>
     </>
   )
@@ -17,13 +23,29 @@ export default CardProject
 const Container = styled.div`
   width: 100%;
   display: flex;
-  padding: 15px 25px;
-  border-radius: 12px;
+  height: 300px;
+  position: relative;
+  border-radius: 0.5rem;
+`
+
+const Overly = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  position: absolute;
+  padding: 1rem 2rem;
   flex-direction: column;
   align-items: flex-start;
-  height: calc(3.5 * 4rem);
+
   justify-content: flex-end;
-  background-color: ${({ theme }) => theme.hover};
+  background: rgba(1, 1, 1, 0.5);
+`
+
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: 0.5rem;
 `
 
 const Title = styled.h1`
