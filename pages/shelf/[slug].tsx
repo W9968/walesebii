dc828a8fa46d53fec60c2524611fc702aa415039
Walee Sebiiy <44040669+nyuuke@users.noreply.github.com>
2021-11-24 23:00:29 +0100

    Update [slug].tsx

--- pages/shelf/[slug].tsx
+++ pages/shelf/[slug].tsx
@@ -1,7 +1,7 @@
 import { useRouter } from 'next/router'
 import type { NextPage } from 'next'
 
-const Slug: NextPage = () => {
+const Slug = () => {
   const router = useRouter()
   const { slug } = router.query
   return <>{`${slug}`.replaceAll('-', ' ')}</>
