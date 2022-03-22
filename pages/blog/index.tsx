08cb75904c33cad63e91bb89a1f1ed58ff9e66de
walee sebiiy <waleesebiiy@gmail.com>
2022-03-22 23:24:32 +0100

    change fetching data to SSR instead of SSG to remove data caching

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -34,7 +34,7 @@ const index: NextPage<staticProps> = ({ payload }) => {
 
 export default index
 
-export async function getStaticProps({ preview = null }) {
+export async function getServerSideProps({ preview = null }) {
   const payload = (await getAllPostsForHome(preview)) || []
   return {
     props: { payload },
