07c19fcfcae418224d5e514b4122eef867a24078
walee sebiiy <waleesebiiy@gmail.com>
2022-03-23 00:11:28 +0100

    incremental static regeneration if you want to keep the pages static for SEO

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -34,9 +34,10 @@ const index: NextPage<staticProps> = ({ payload }) => {
 
 export default index
 
-export async function getServerSideProps({ preview = null }) {
+export async function getStaticProps({ preview = null }) {
   const payload = (await getAllPostsForHome(preview)) || []
   return {
     props: { payload },
+    revalidate: 10,
   }
 }
